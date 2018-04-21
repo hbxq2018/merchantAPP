c1acbfbcaf3dd5822a74157bc33fa687b7333114
yangfeng <1138391635@qq.com>
2018-04-21 17:41:18 +0800

    杨锋提交测试

--- src/App.vue
+++ src/App.vue
@@ -7,20 +7,21 @@
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style>
-html, body {
-    margin: 0;
-    width: 100%;
-    height: 100%;
-} 
+html,
+body {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
 #app {
   width: 100%;
   height: 100%;
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
