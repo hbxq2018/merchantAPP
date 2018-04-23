2981d28cc97c76e393c080aa3fd1fe902c0fac0a
webUserBaby <706353323@qq.com>
2018-04-23 15:12:43 +0800

    测试配置用户名邮箱是否成功

--- src/components/login/settlein.vue
+++ src/components/login/settlein.vue
@@ -1,30 +1,29 @@
 <template>
   <div class="settle">
-      {{msg}}
     <router-link to="review">
-            <mt-button type="default">下一页面</mt-button>
-        </router-link>
+        <mt-button type="default">下一页面</mt-button>
+    </router-link>
   </div>
 </template>
 <script>
 export default {
-  name:'Settle',
-  data(){
-      return {
-          msg:"表单页面"
-      }
+  name: "Settle",
+  data() {
+    return {
+      msg: "表单页面"
+    };
   }
-}
+};
 </script>
 
 <style lang="less">
-    .settle{
-        width: 100%;
-        background: yellow;
-        min-height: 400px;
-        color: blueviolet;
-        font-size: 44px;
-    }
+.settle {
+  width: 100%;
+  background: yellow;
+  min-height: 400px;
+  color: blueviolet;
+  font-size: 44px;
+}
 </style>
 
 
