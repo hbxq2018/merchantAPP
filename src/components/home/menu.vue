c805b92f8736ac088b9baa281b8b390d91919a72
luyong <13971489895@163.com>
2018-04-23 14:22:48 +0800

    other

--- src/components/home/menu.vue
+++ src/components/home/menu.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
+  <div class="menu">
+      <div class="menu_top">
+           <img class="eaves" src="../../common/images/eaves.png" alt="顶部图片">
+            <div class="info">
+                <img class="avatar" :src='src' alt="用户头像">
+                <div class="username">
+                    <p class="name"></p>
+                    <p class="heat">人气{{}}</p>
+                </div>
+            </div>
+      </div>
       {{msg}}
        <router-link to="">
             <mt-button type="default">首页</mt-button>
@@ -12,13 +22,49 @@
 
 <script>
 export default {
-    name: 'Login',
+    name: 'Menu',
     data(){
         return {
-            msg:'login'
+            msg:'menu',
+            src:'../../../common/images/timg (2).jpg',
+            userdata:{
+                src:'../../common/images/timg (2).jpg',
+                name:'恩施映像',
+                heat:'1382',
+            }
         }
     }
 }
 </script>
+
+<style lang="less">
+    .menu{
+        .menu_top{
+            width: 100%;
+            &>.eaves{
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 111px;
+            }
+            &>.info{
+                height: 133px;
+                padding-top: 134px;
+                padding-left:39px;
+                padding-right: 39px;
+                background: #FC5E2D;
+                &>.avatar{
+                    width: 100px;
+                    height: 100px;
+                    float: left;
+                    border-radius: 50%;
+                    margin-left: 31px;
+                }
+            }
+        }
+    }
+</style>
+
 
 
